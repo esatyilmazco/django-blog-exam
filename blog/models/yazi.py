c660e489568db89fccb5958e79c959a0c16ce21c
Esat Yılmaz <esatcan.yilmaz@akinon.com>
2021-11-15 15:18:33 +0300

    yazi admin paneli düzenlendi

--- blog/models/yazi.py
+++ blog/models/yazi.py
@@ -3,12 +3,14 @@ from django.db import models
 from autoslug import AutoSlugField
 from blog.models import KategoriModel
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+
 
 class Yazilar(models.Model):
     
     resim=models.ImageField(upload_to='yazi_resimleri')
     baslik=models.CharField(max_length=50)
-    icerik=models.TextField()
+    icerik=RichTextField()
     olusturulma_tarihi=models.DateTimeField(auto_now_add=True)
     duzenlenme_tarihi=models.DateTimeField(auto_now=True)
     slug=AutoSlugField(populate_from='baslik',unique=True)
@@ -18,4 +20,7 @@ class Yazilar(models.Model):
     class Meta:
         verbose_name = 'Yazi'
         verbose_name_plural = 'Yazilar'
-        db_name='Yazi'
+        db_table = 'Yazi'
+        
+    def __str__(self):
+        return self.baslik
