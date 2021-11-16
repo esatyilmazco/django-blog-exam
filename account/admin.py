3904bd34139ad0c458573d8c67f0adb6cf2841b7
Esat Yılmaz <esatcan.yilmaz@akinon.com>
2021-11-16 10:20:01 +0300

    duzenlemeler yapıldı

--- account/admin.py
+++ account/admin.py
@@ -3,18 +3,15 @@ from django.contrib.auth.admin import UserAdmin
 from account.models import CustomUserModel
 
 
+@admin.register(CustomUserModel)
 class CustomAdmin(UserAdmin):
-    model=CustomUserModel
     list_display=('username', 'email')
     fieldsets = UserAdmin.fieldsets + (
         ('Avatar Değiştirme Alanı',{
             'fields':['avatar']
-        }),
+        }),   
     )
     
-    
-admin.site.register(CustomUserModel,CustomAdmin)
-
 
 # Register your models here.
 
--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,7 @@ from blog.models import(
 # Register your models here.
 admin.site.register(KategoriModel)
 
+@admin.register(Yazilar)
 class YazilarAdmin(admin.ModelAdmin):
     search_fields =('baslik','icerik')
     list_display = (
@@ -12,23 +13,20 @@ class YazilarAdmin(admin.ModelAdmin):
     )
 
 
-admin.site.register(Yazilar,YazilarAdmin)
 
-
+@admin.register(YorumModel)
 class YorumAdmin(admin.ModelAdmin):
     search_fields=('yazan_username',)
 
     list_display = (
-        'yazan','olusturulma_tarihi','guncelleme_tarihi'
+        'yazan','olusturulma_tarihi','duzenlenme_tarihi'
     )
 
-admin.site.register(YorumModel,YorumAdmin)
 
-
+@admin.register(IletisimModel)
 class IletisimAdmin(admin.ModelAdmin):
     list_display = (
         'email','olusturulma_tarihi'
     )
     search_fields =('email',)
 
-admin.site.register(IletisimModel,IletisimAdmin)
