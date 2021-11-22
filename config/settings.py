3de465e68689ffc54c5b66af718bbb6069fac7c1
Esat Yılmaz <esatcan.yilmaz@akinon.com>
2021-11-22 13:42:52 +0300

    settings ayarlamalari  yapildi

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,10 @@
 import os
 from pathlib import Path
+import environ
 
+
+env = environ.Env()
+environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_+h28_t&53o5$(p)lf-()yq1ss#jd%76t=o@q!g+rg3ka(6%9t'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
